14a03ff399296ae0dc2241f8c2bc644b8468425c
Kostin Ivan <mrironheart@gmail.com>
2026-03-08 01:13:04 +0300

    Добавил класс для генерации случайных значений

---
+++ src/test/java/testdata/TestDataProvider.java
@@ -0,0 +1,39 @@
+package testdata;
+
+import com.github.javafaker.Faker;
+
+import java.util.Random;
+
+/**
+ * Класс для формирования тестовых данных в тестах
+ */
+public class TestDataProvider {
+    private static final Faker FAKER = new Faker();
+
+    /**
+     * Метод возвращает случайное имя из генератора JavaFaker
+     *
+     * @return String
+     */
+    public static String getRandomName() {
+        return FAKER.name().name();
+    }
+
+    /**
+     * Метод возвращает случайный пароль из генератора JavaFaker
+     *
+     * @return String
+     */
+    public static String getRandomPassword() {
+        return FAKER.internet().password();
+    }
+
+    /**
+     * Метод возвращает случайный email адрес из генератора JavaFaker
+     *
+     * @return String
+     */
+    public static String getRandomEmail() {
+        return FAKER.internet().emailAddress();
+    }
+}
